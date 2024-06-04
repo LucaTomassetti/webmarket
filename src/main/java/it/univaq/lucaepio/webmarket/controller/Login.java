f8946e45c80c2b1853dec76cf2a555d7109aa1c1
LucaTomassetti <luca.tomassetti02@gmail.com>
2024-06-04 18:36:56 +0200

    login controller added

---
+++ src/main/java/it/univaq/lucaepio/webmarket/controller/Login.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package it.univaq.lucaepio.webmarket.controller;
+
+import it.univaq.f4i.iw.framework.result.TemplateManagerException;
+import it.univaq.f4i.iw.framework.result.TemplateResult;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author lucat
+ */
+public class Login extends HttpServlet {
+
+   private void action_default(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
+        TemplateResult res = new TemplateResult(getServletContext());
+        res.activate("login.html", request, response);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, TemplateManagerException {
+
+       try {
+           action_default(request, response);
+       } catch (IOException ex) {
+           Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+       }
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Login servlet";
+    }// </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       try {
+           processRequest(request, response);
+       } catch (TemplateManagerException ex) {
+           Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+       }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       try {
+           processRequest(request, response);
+       } catch (TemplateManagerException ex) {
+           Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+       }
+    }
+
+}
