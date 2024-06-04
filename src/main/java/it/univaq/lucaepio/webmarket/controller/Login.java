8283bae2dd45d73accc742cdd2823b76292e5f31
LucaTomassetti <luca.tomassetti02@gmail.com>
2024-06-04 19:52:48 +0200

    servlet login aggiornata e template html mappati

--- src/main/java/it/univaq/lucaepio/webmarket/controller/Login.java
+++ src/main/java/it/univaq/lucaepio/webmarket/controller/Login.java
@@ -6,6 +6,7 @@ package it.univaq.lucaepio.webmarket.controller;
 
 import it.univaq.f4i.iw.framework.result.TemplateManagerException;
 import it.univaq.f4i.iw.framework.result.TemplateResult;
+import it.univaq.f4i.iw.framework.security.SecurityHelpers;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,16 +25,40 @@ public class Login extends HttpServlet {
         TemplateResult res = new TemplateResult(getServletContext());
         res.activate("login.html", request, response);
     }
+    
+    private void action_login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("u");
+        String password = request.getParameter("p");
 
+        if (!username.isEmpty() && !password.isEmpty()) {
+            response.sendRedirect("homepage");
+        }else{
+            response.sendRedirect("_404");
+        }
+    }
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws javax.servlet.ServletException
+     * @throws it.univaq.f4i.iw.framework.result.TemplateManagerException
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, TemplateManagerException {
-
-       try {
-           action_default(request, response);
-       } catch (IOException ex) {
-           Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-       }
-
+        try {
+            if (request.getParameter("login") != null) {
+                action_login(request, response);
+            } else {
+                String https_redirect_url = SecurityHelpers.checkHttps(request);
+                request.setAttribute("https-redirect", https_redirect_url);
+                action_default(request, response);
+            }
+        } catch (IOException ex){
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);  
+        }
     }
 
     /**
@@ -82,4 +107,9 @@ public class Login extends HttpServlet {
        }
     }
 
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
 }
